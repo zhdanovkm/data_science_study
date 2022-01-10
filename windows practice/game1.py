8deae0ac3e42c471757af37bc18ac7afc477f1ec
Kirill Zhdanov <zhdanovkm@gmail.com>
2022-01-10 10:14:29 +0300

    v1

--- windows practice/game1.py
+++ windows practice/game1.py
@@ -5,7 +5,7 @@ count = 0
 
 while True:
     count += 1
-    predict_number = int(input("Угадай число от 1 до 100"))
+    predict_number = int(input("Угадай число от 1 до 100: "))
 
     if predict_number > number:
         print("Число должно быть меньше!")
