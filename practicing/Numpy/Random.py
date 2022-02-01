197f0c3b8f06675fd860b35aae08bce312dd596c
ZhdanovKM <zhdanovkm@gmail.com>
2022-02-02 01:04:06 +0300

    v2

--- practicing/Numpy/Random.py
+++ practicing/Numpy/Random.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy import rand
 
 random = np.random.rand() #случайное число от 0 до 1
 print(random)
@@ -13,5 +12,56 @@ print("\n", random)
 random = np.random.rand(2,4)
 print("\n", random)
 
-random = np.random.rand(2, 3, 4, 10, 12, 23)
+#random = np.random.rand(2, 3, 4, 10, 12, 23) - 6и мерный массив, много места
+#print("\n", random)
+
+shape = (2,3)
+random = np.random.rand(*shape) #кортеж обрабатывается только в распакованном виде *
 print("\n", random)
+
+random = np.random.sample(shape) #для этой функции не нужна распаковка кортежа
+print("\n", random)
+
+random = np.random.uniform()
+print("\n", random)
+random = np.random.uniform(-5, 10)
+print("\n", random)
+random = np.random.uniform(-5, 10, size=3) #верхняя, нижняя граница + размер
+print("\n", random)
+random = np.random.uniform(-5, 10, size=(3,2)) 
+print("\n", random)
+
+random = np.random.randint(4, size=(2,3)) #randint(low, high=None, size=None, dtype=int)
+print("\n", random)
+
+random = np.random.randint(6, 12, size=(3,3))
+print("\n", random)
+
+arr = np.arange(6)
+print(arr)
+print(np.random.shuffle(arr)) #Просто перемешать все числа в массиве 
+print(arr) 
+
+playlist = ["The Beatles", "Pink Floyd", "ACDC", "Deep Purple"]
+shuffled = np.random.permutation(playlist) #Чтобы получить новый перемешанный массив, а исходный оставить без изменений
+print(shuffled)
+print(playlist)
+
+print(np.random.permutation(10)) #Перемешать набор чисел от 0 до n-1 без использования функции arrange
+
+#выбрать 2х случайных людей из списка без повторений:
+workers = ['Ivan', 'Nikita', 'Maria', 'John', 'Kate']
+choice = np.random.choice(workers, size=2, replace=False)
+print(choice)
+
+#для воспроизводимого кода необходимо использовать seed:
+np.random.seed(23)
+print(np.random.randint(10, size=(3,4))) 
+
+np.random.seed(100)
+print(np.random.randint(10, size=3))
+print(np.random.randint(10, size=3))
+print(np.random.randint(10, size=3))
+
+ar=np.arange(2,17,2)
+print(ar)
